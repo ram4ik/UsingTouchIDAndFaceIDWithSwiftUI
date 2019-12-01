c3f24d8cf474f1e0a5160a236c76824ee4e3168d
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2019-12-01 10:45:09 +0200

    Using TouchID and Face ID with SwiftUI

--- UsingTouchIDAndFaceIDWithSwiftUI/ContentView.swift
+++ UsingTouchIDAndFaceIDWithSwiftUI/ContentView.swift
@@ -6,11 +6,43 @@
 //  Copyright © 2019 Ramill Ibragimov. All rights reserved.
 //
 
+import LocalAuthentication
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isUnlocked = false
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            if self.isUnlocked {
+                Text("Unlocked")
+            } else {
+                Text("Locked")
+            }
+        }
+        .onAppear(perform: authenticate)
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "We need to unlock your data."
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                DispatchQueue.main.async {
+                    if success {
+                        // authenticated successfully
+                        self.isUnlocked = true
+                    } else {
+                        // there was a problem
+                    }
+                }
+            }
+        } else {
+            // no biometrics
+        }
     }
 }
 
